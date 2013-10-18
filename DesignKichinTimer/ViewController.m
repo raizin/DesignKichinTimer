d87fec2dca97aff98b4a8e8b8eaf244bbe06b772
Amasaki Raijin <raizin64@fmail.com>
2013-10-18 23:54:15 +0900

    初期作成からの試験的なコミット

--- DesignKichinTimer/ViewController.m
+++ DesignKichinTimer/ViewController.m
@@ -14,6 +14,25 @@
 
 @implementation ViewController
 
+
+- (void)viewWillAppear:(BOOL)animated
+{
+  self.view.layer.backgroundColor = [UIColor whiteColor].CGColor;
+  self.view.layer.cornerRadius = 50.0;
+  self.view.layer.frame = CGRectInset(self.view.layer.frame, 20, 20);
+  
+  //  self.view.layer.contents = (id)[UIImage imageNamed:@"bg01.jpg"].CGImage;
+  CALayer *sublayer = [CALayer layer];
+  sublayer.backgroundColor = [UIColor blueColor].CGColor;
+  sublayer.shadowOffset = CGSizeMake(0, 3);
+  sublayer.shadowRadius = 5.0;
+  sublayer.shadowColor = [UIColor blackColor].CGColor;
+  sublayer.shadowOpacity = 0.8;
+  sublayer.frame = CGRectMake(30, 30, 128, 192);
+  [self.view.layer addSublayer:sublayer];
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
