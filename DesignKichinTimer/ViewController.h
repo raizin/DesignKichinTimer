74df8ee0879a11d0a3ed416bf9e341a5ea055c92
Amasaki Raijin <raizin64@fmail.com>
2013-10-24 19:01:02 +0900

    ローカライズ以外を反映

--- DesignKichinTimer/ViewController.h
+++ DesignKichinTimer/ViewController.h
@@ -8,6 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+  
+  //  UILabel *nowDate;
+  //  UIView  *nowDateView;
+  //  NSString *dispStr;
+  //  UIButton *setBtn;
+  //  BOOL flgViewSeco;
+  //  BOOL flgViewDate;
+  //  BOOL flgBlink;
+  //  BOOL blinkCanma;
+  
+  //  CGPoint centerLocation_Date;
+  //  CGPoint centerLocation_Time;
+  
+  // カウンター表示ラベル定義
+  UILabel *cntLabel;
+  
+  // カウンター表示エリア横幅,縦幅
+  int cntW;
+  int cntH;
+  
+  // カウンター表示エリア
+  CALayer *sublayer;
+  
+}
 
 @end
