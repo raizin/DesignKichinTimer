7d5745bb0f5b3cced724db901556ab8073d5be3a
Amasaki Raijin <raizin64@gmail.com>
2014-03-26 08:59:29 +0900

    カウントアップルーチンの見直しー＞バグ修正

--- DesignKichinTimer/AppDelegate.m
+++ DesignKichinTimer/AppDelegate.m
@@ -10,24 +10,34 @@
 
 @implementation AppDelegate
 
-
-// 起動回数を返す Globalな関数
+/**
+ * 起動回数を返す Globalな関数
+ */
 - (int)getAppLauchedCount
 {
   NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
   return (int)[ud integerForKey:@"appLauchedCount"];
 }
 
-// 起動回数を保存
-- (void)setAppLauchedCount:(int)cnt
+/**
+ * 起動回数のカウントアップ
+ * 2147483647(=INT_MAX) を超えそうになったら 1 に戻す。-> デフォルトでは、-2147483647に戻る
+ */
+- (void)setAppLauchedCount
 {
   NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
-
+  
+  int cnt = (int)[ud integerForKey:@"appLauchedCount"];
+  if (cnt == INT_MAX) {
+    cnt = 1;
+  }
   cnt++;
   
   [ud setInteger:cnt forKey:@"appLauchedCount"];
   [ud synchronize];
 }
+
+
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -37,23 +47,9 @@
   [audioSession setCategory:AVAudioSessionCategorySoloAmbient error:NULL];
   [audioSession setActive:YES error:NULL];
   
-  int appLaunchedCountValue = [self getAppLauchedCount];
+  [self setAppLauchedCount]; // 起動回数カウントUP
+
   
-  // 99999回以上はカウントしない。
-  if ( appLaunchedCountValue < 99999 ) {
-    [self setAppLauchedCount:appLaunchedCountValue];
-  }
-/*
-  if ( appLaunchedCountValue == 5 ) {
-    UIAlertView *alertView = [[UIAlertView alloc]
-                              initWithTitle:[NSString stringWithFormat:@"%@",NSLocalizedString(@"AlertTtl", nil)]
-                                    message:[NSString stringWithFormat:@"%@",NSLocalizedString(@"AlertMsg", nil)]
-                                   delegate:self
-                          cancelButtonTitle:[NSString stringWithFormat:@"%@",NSLocalizedString(@"AlertNG", nil)]
-                          otherButtonTitles:[NSString stringWithFormat:@"%@",NSLocalizedString(@"AlertOK", nil)],nil];
-    [alertView show];
-  }
-*/  
   // Override point for customization after application launch.
   return YES;
 }
@@ -61,31 +57,65 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+  //非アクティブ or 割り込み（電話が掛かってきた時など）
+  LOG(@"%s",__func__);
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  //アプリがバックグラウンドになった時
+  LOG(@"%s",__func__);
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-  // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-  
+  //アプリがバックグラウンドからフォアグラウンドになる直前
+  LOG(@"%s",__func__);
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  
+  // アプリがアクティブになった時
+  LOG(@"%s",__func__);
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  // アプリ終了
+  LOG(@"%s",__func__);
 }
 
+/*
+ - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+ {
+ NSLog(@"connection didFailWithError");
+ exit(0);
+ 
+ 
+ if(error.code==-1009){
+ 
+ exit(0);
+ 
+ }
+ 
+ }
+ */
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
